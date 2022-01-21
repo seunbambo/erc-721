98da280c99f096e4befcdf82bdaf70011dacef97
Oluwaseun Adebambo <seun.bambo@gmail.com>
2022-01-21 19:50:28 +0100

    First commit

---
+++ contracts/ERC721.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.8.2;
+
+contract ERC721 {
+
+mapping(address => uint256) internal _balances;
+
+mapping(uint256 => address) internal _owners;
+
+    // Returns the number of NFTs assigned to an owner
+    function balanceOf(address owner) public view returns(uint256) {
+        require(owner != address(0), "Address is zero");
+        return _balances[owner];
+    }
+
+    // Finds the owner of an NFT
+    function ownerOf(uint256 tokenId) public view returns(address) {
+        address owner = _owners[tokenId];
+        require(owner != address(0), "TokenId does not exist");
+        return owner;
+    }
+}
