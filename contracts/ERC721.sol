61a71deecabce2889532bfb67e96a7fe5ef49b22
Oluwaseun Adebambo <seun.bambo@gmail.com>
2022-01-21 20:02:18 +0100

    Added setApprovalForAll and isApprovedForAll functions

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -2,20 +2,35 @@ pragma solidity ^0.8.2;
 
 contract ERC721 {
 
-mapping(address => uint256) internal _balances;
+    event ApprovalForAll(address indexed _owner, address indexed _operator, bool _approved);
 
-mapping(uint256 => address) internal _owners;
+    mapping(address => uint256) internal _balances;
 
-    // Returns the number of NFTs assigned to an owner
+    mapping(uint256 => address) internal _owners;
+
+    mapping(address => mapping(address => bool)) private _operatorApprovals;
+
+    // Returns the number of NFTs assigned to an owner.
     function balanceOf(address owner) public view returns(uint256) {
         require(owner != address(0), "Address is zero");
         return _balances[owner];
     }
 
-    // Finds the owner of an NFT
+    // Finds the owner of an NFT.
     function ownerOf(uint256 tokenId) public view returns(address) {
         address owner = _owners[tokenId];
         require(owner != address(0), "TokenId does not exist");
         return owner;
+    }
+
+    // Enables or disables an operator to manager all of msg.senders assets.
+    function setApprovalForAll(address operator, bool approved) public {
+        _operatorApprovals[msg.sender][operator] = approved;
+        emit ApprovalForAll(msg.sender, operator, approved);
+    }
+
+    // Checks if an address is an operator for another address
+    function isApprovedForAll(address owner, address operator) public view returns(bool) {
+        return _operatorApprovals[owner][operator];
     }
 }
