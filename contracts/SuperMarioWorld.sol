60099c17fb89bcc162caf238604ef99b6790e213
Oluwaseun Adebambo <seun.bambo@gmail.com>
2022-01-25 16:31:00 +0100

    Added SuperMarioWorld contract

---
+++ contracts/SuperMarioWorld.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.8.2;
+
+import "./ERC721.sol";
+
+contract SuperMarioWorld is ERC721 {
+    string public name;
+
+    string public symbol;
+
+    uint256 public tokenCount;
+
+    mapping(uint256 => string) private _tokenURIs;
+
+    constructor(string memory _name, string memory _symbol) {
+        name = _name;
+        symbol = _symbol;
+    }
+
+    // Returns a URL that points to the metadata
+    function tokenURI(uint256 tokenId) public view returns(string memory) {
+        require(_owners[tokenId] != address(0), "TokenID does not exist.");
+        return _tokenURIs[tokenId];
+    }
+}
