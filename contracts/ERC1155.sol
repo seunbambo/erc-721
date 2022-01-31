1ac1a5488c3eddeed2b80d84c858ff7ada6ccb6f
Oluwaseun Adebambo <seun.bambo@gmail.com>
2022-01-31 14:14:01 +0100

    Implemented isApprovalForAll and setApprovalForAll functions for ERC-1155

--- contracts/ERC1155.sol
+++ contracts/ERC1155.sol
@@ -1,8 +1,15 @@
 pragma solidity ^0.8.2;
 
 contract ERC1155 {
+
+    event ApprovalForAll(address indexed _owner, address indexed _operator, bool _approved);
+
     // Mapping from TokenID to account balances.
     mapping(uint256 => mapping(address => uint256)) internal _balances;
+
+    // Mapping from account to operator approvals.
+    mapping(address => mapping(address => uint256)) private _operatorApprovals;
+
     // Gets the balance of an accounts tokens.
     function balanceOf(address account, uint256 id) public view returns(uint256) {
         require(account != address(0), "Address is zero.");
@@ -19,5 +26,16 @@ contract ERC1155 {
         }
 
         return batchBalances;
+    }
+
+    // Checks is an address is an operator for another address.
+    function isApprovedForAll(address account, address operator) public returns(bool) {
+        return _operatorApprovals[account][operator];
+    }
+
+    // Enables or disables operator to manage all of msg.senders assets
+    function setApprovalForAll(address operator, bool approved) public {
+        _operatorApprovals[msg.sender][operator] = approved;
+        emit ApprovalForAll(msg.sender, operator, approved);
     }
 }
